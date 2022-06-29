78957b87a2d4aa53f171963f05be74ce326073a1
Justin Awrey <jawrey@dialpad.com>
2022-06-28 18:59:55 -0700

    a start

--- packages/lib/src/dev/expose-development.ts
+++ packages/lib/src/dev/expose-development.ts
@@ -2,6 +2,7 @@ import { parseExposeOptions } from '../utils'
 import { parsedOptions } from '../public'
 import type { VitePluginFederationOptions } from 'types'
 import type { PluginHooks } from '../../types/pluginHooks'
+import type { ViteDevServer } from '../../types/viteDevServer'
 
 export function devExposePlugin(
   options: VitePluginFederationOptions
@@ -9,6 +10,9 @@ export function devExposePlugin(
   parsedOptions.devExpose = parseExposeOptions(options)
 
   return {
-    name: 'originjs:expose-development'
+    name: 'originjs:expose-development',
+
+    // eslint-disable-next-line @typescript-eslint/no-empty-function
+    configureServer(server: ViteDevServer) {}
   }
 }
